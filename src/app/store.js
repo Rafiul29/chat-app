3c87fe66a9b3d33567dc6433ffafb3ef615e3971
Rafiul29 <rafiulislam4472@gmial.com>
2024-08-07 12:14:52 +0600

    create api slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import authSliceReducer from "../features/auth/authSlice";
+import conversationsReducer from "../features/conversations/conversationsSlice";
+import messagesReducer from "../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
+   devTools:!process.env.NODE_ENV==='production',
+   middleware:(getDefaultMiddlewares)=>getDefaultMiddlewares().concat(apiSlice.middleware)
 });
---
+++ src/features/messages/messagesSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const initialState={
+
+}
+
+const messagesSlice=createSlice({
+     initialState,
+    reducers:{
+
+    }
+})
+
+
+export const {}=messagesSlice.actions
+export default messagesSlice.conversationssSlice
