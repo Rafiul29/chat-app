07b391e9025d5a75c32d19445db1c5be14e3a999
Rafiul29 <rafiulislam4472@gmail.com>
2024-08-08 16:19:34 +0600

    chat header added

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,23 @@
-export default function ChatHead({ avatar, name }) {
-    return (
-        <div className="relative flex items-center p-3 border-b border-gray-300">
-            <img
-                className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
-            />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
-        </div>
-    );
+import { useSelector } from "react-redux";
+import getAvater from 'gravatar-url'
+
+export default function ChatHead({ messages }) {
+
+  const { sender, receiver } = messages || {};
+  const { user } = useSelector((state) => state.auth) || {};
+  const { email } = user || {};
+
+  const partnerEmail=sender.email===email? receiver.email : sender.email
+  const partnerName=sender.email===email? receiver.name : sender.name
+  
+  return (
+    <div className="relative flex items-center p-3 border-b border-gray-300">
+      <img
+        className="object-cover w-10 h-10 rounded-full"
+        src={getAvater(partnerEmail)}
+        alt={partnerName}
+      />
+      <span className="block ml-2 font-bold text-gray-600">{partnerName}</span>
+    </div>
+  );
 }
